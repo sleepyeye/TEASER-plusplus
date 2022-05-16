f61c239c84fe5d1a03ae1563ec26345637bd99b4
sleepyeye <wonjunlee.0729@gmail.com>
2022-05-16 13:55:12 +0900

    Change eigen version to 3.4

--- cmake/teaserppConfig.cmake
+++ cmake/teaserppConfig.cmake
@@ -1,7 +1,7 @@
 get_filename_component(TEASERPP_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
-find_dependency(Eigen3 3.3 REQUIRED)
+find_dependency(Eigen3 3.4 REQUIRED)
 find_dependency(OpenMP REQUIRED)
 
 include("${TEASERPP_CMAKE_DIR}/teaserppTargets.cmake")
