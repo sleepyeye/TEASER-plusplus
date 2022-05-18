98c1f810f088344e6584ee27d250d3b95d99f0db
sleepyeye <wonjunlee.0729@gmail.com>
2022-05-18 10:51:58 +0900

    add ENABLE_OPENMP option

--- cmake/teaserppConfig.cmake
+++ cmake/teaserppConfig.cmake
@@ -2,7 +2,7 @@ get_filename_component(TEASERPP_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(CMakeFindDependencyMacro)
 
 find_dependency(Eigen3 3.4 REQUIRED)
-find_dependency(OpenMP REQUIRED)
+find_dependency(OpenMP)
 
 include("${TEASERPP_CMAKE_DIR}/teaserppTargets.cmake")
 
